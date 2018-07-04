1c3380529cf5c5db054f3458eab1c87ed6548c56
Wwar-lock <adityasharma3204@gmail.com>
2018-07-04 18:18:08 +0530

    add association and add debugger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+    validates :description, presence: true, length: { minimum:5, maximim: 300 }
+    validates :user_id, presence: true
 end
